daf820affccd57c245ec5a27c58eb8537cb579c3
Vaceff Vlad <vaceffv@gmail.com>
2024-08-10 16:53:12 +0300

    feature: added logo and path printing

--- basic_filesystem_pOS/basic_filesystem_pOS/main.c
+++ basic_filesystem_pOS/basic_filesystem_pOS/main.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <direct.h>
+#include <limits.h>
+#include <errno.h>
+#include <string.h>
+
+#define BUFFER_SIZE 1024
+
+void logo() {
+	printf(" _____ _        _  ____   _____ \n");
+	printf("|  __ (_)      | |/ __ \\ / ____|\n");
+	printf("| |__) | ______| | |  | | (___  \n");
+	printf("|  ___/ |_  / _` | |  | |\\___ \\ \n");
+	printf("| |   | |/ / (_| | |__| |____) |\n");
+	printf("|_|   |_/___\\__,_|\\____/|_____/ \n\n");
+}
+
+
 
 int main(int argc, char* argv[]) {
-	print("Hello world %s", argv[0]);
+	logo();
+	char workingDirectory[BUFFER_SIZE] = { '\0' };
+	char userInput[BUFFER_SIZE] = { '\0' };
+	while (strcmp(userInput, "exit") != 0) {
+		if (_getcwd(workingDirectory, sizeof(workingDirectory)) != NULL) {
+			printf("%s>", workingDirectory);
+		}
+		else {
+			perror("_getcwd() error. Path could not be retrieved");
+			return 1;
+		}
+		scanf_s("%1023s", &userInput, BUFFER_SIZE - 1);
+	}
 	return 0;
 }
